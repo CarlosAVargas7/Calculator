96099548906d64abc4d28021a7cfadadf0f4ba40
Carlos A. Vargas <karltfx15@outlook.com>
2025-09-11 18:53:48 -0500

    v2.1.0

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,6 @@ const botonmas = document.querySelector<HTMLButtonElement>('#bmas');
 const botonmen = document.querySelector<HTMLButtonElement>('#bmen');
 let resultado = document.querySelector<HTMLOutputElement>('#res');
 
-
-
-
 botonmas?.addEventListener('click', () => {
     console.log('Boton mas se lee');
     const actual = Number(resultado?.textContent);
@@ -19,6 +16,7 @@ botonmas?.addEventListener('click', () => {
 botonmen?.addEventListener('click', () => {
     console.log('Boton men se lee');
     const valor = Number(resultado?.textContent);
-    resultado!.textContent= String(valor -1);
+    resultado!.textContent = String(valor - 1);
     console.log(valor);
 });
+
